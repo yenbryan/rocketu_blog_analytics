2e9ff2d6a78254444df117eea1e8d34aee552e18
Rudy Mutter <rudy@yetihq.com>
2014-10-18 16:50:06 -0700

    fixing data migration

--- blog/migrations/0002_auto_20141018_2245.py
+++ blog/migrations/0002_auto_20141018_2245.py
@@ -18,23 +18,38 @@ def create_initial_data(apps, schema_editor):
     django_tag = Tag.objects.create(name='Django')
 
     Post = apps.get_model("blog", "Post")
-    post_one = Post.objects.create(created=now() - timedelta(days=14),
-                                   body='Django makes it easy to get from 0 to working application in weeks!',
+    post_one = Post.objects.create(body='Django makes it easy to get from 0 to working application in weeks!',
                                    title='Why I Love Django',
                                    author=anna)
+    # Need to set created after already saved
+    post_one.created = now() - timedelta(days=14)
+    post_one.save()
     post_one.tags.add(programming_tag, django_tag)
 
-    post_two = Post.objects.create(created=now() - timedelta(days=7),
-                                   body='In Django 1.7 we were introduced to built-in migrations!',
+    post_two = Post.objects.create(body='In Django 1.7 we were introduced to built-in migrations!',
                                    title='How Django Got Better',
                                    author=anna)
+    post_two.created = now() - timedelta(days=7)
+    post_two.save()
     post_two.tags.add(programming_tag, awesomeness_tag, django_tag)
 
-    post_three = Post.objects.create(created=now() - timedelta(days=21),
-                                     body="Are they talking about Django Reinhardt or Django Unchained?",
+    post_three = Post.objects.create(body="Are they talking about Django Reinhardt or Django Unchained?",
                                      title="I've Never Heard of Django",
                                      author=george)
+    post_three.created = now() - timedelta(days=21)
+    post_three.save()
     post_three.tags.add(awesomeness_tag, django_tag)
+
+
+def remove_initial_data(apps, schema_editor):
+    Author = apps.get_model("blog", "Author")
+    Author.objects.all().delete()
+
+    Tag = apps.get_model("blog", "Tag")
+    Tag.objects.all().delete()
+
+    Post = apps.get_model("blog", "Post")
+    Post.objects.all().delete()
 
 
 class Migration(migrations.Migration):
@@ -44,5 +59,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(create_initial_data)
+        migrations.RunPython(create_initial_data, reverse_code=remove_initial_data)
     ]
